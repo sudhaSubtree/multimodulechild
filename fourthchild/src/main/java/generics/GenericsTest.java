a2b0590b9345c96c36b264c4e7eac31b257fe94e
Hosagoudar <sudha.hosagoudar@hp.com>
2023-04-06 11:32:44 +0530

    Multimodule Test

--- fourthchild/src/main/java/generics/GenericsTest.java
+++ fourthchild/src/main/java/generics/GenericsTest.java
@@ -4,6 +4,7 @@ public class GenericsTest {
 
 	public void testmultimodulechild(String msg) {
 		System.out.println("Testing the flow");
+		System.out.println("Testing the flow 2");
 		System.out.println(msg);
 	}
 }
